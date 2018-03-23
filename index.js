07c502b0ea57675e8f25a3eba604c7ca6a733440
leemove <923522454@qq.com>
2018-03-23 17:36:39 +0800

    update

--- index.js
+++ index.js
@@ -2,25 +2,44 @@ const config = require('./config.json')
 const phantom = require('phantom')
 const _ = require('lodash')
 const fs = require('fs')
+const os = require('os')
 const cheerio = require('cheerio')
-// var mongoose = require('mongoose')
-// mongoose.connect('mongodb://localhost:27017');
-// var db = mongoose.connection;
-// db.once('open', function() {
-//   console.log('数据库连接')
-// });
+var mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/test');
+var db = mongoose.connection;
+db.once('open', function() {
+  console.log('数据库连接')
+});
+
+const houseSchema = mongoose.Schema({
+  title: String,
+  updateTime: Date,
+  link: String
+})
+
+const House = mongoose.model('House', houseSchema)
 
 let pageCount = 0
-
+let saveCount = 0
+const maxPage = 1
 async function main(){
-  const maxPage = 1 // 抓取的页面数
-  const pageStarts = _.range(maxPage).map(item => item *50)
+// 抓取的页面数
+  const pageStarts = _.range(maxPage).map(item => item *25)
   // 放入一个数组 开始并行执行请求
   const proArr = pageStarts.map(item => getPage(item))
   try {
     let res = await Promise.all(proArr)
     res.forEach(element => {
-    const $ = cheerio.load(element.content)
+      saveData(element.content)
+    });
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+async function saveData (content) {
+  try {
+    const $ = cheerio.load(content)
     const trs = $('table.olt td.title a')
     trs.each((index, element) => {
       element = cheerio(element)
@@ -29,15 +48,18 @@ async function main(){
       const parent = element.parent().parent()
       const timeStr = cheerio(parent).find('td.time').text()
       const time = new Date('2018-' + timeStr)
-      console.log({title, herf, time})
+      const house = new House({title, link: herf, updateTime: time})
+      house.save(function () {
+        saveCount++
+        if (saveCount === maxPage) {
+          mongoose.disconnect()
+        }
+      });
     })
-      // fs.writeFile(__dirname + 'dist/' + 'note' + element.start, element.content)
-    });
   } catch (error) {
-    console.log(error)
+    console.error('数据保存失败')
   }
 }
-
 
 async function getPage(start) {
   const instance = await phantom.create();
