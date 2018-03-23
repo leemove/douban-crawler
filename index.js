fa56cffd7d821d35db7ed968bef12ee14a07c89e
leemove <923522454@qq.com>
2018-03-23 16:27:41 +0800

    增加提示

--- index.js
+++ index.js
@@ -4,14 +4,17 @@ const phantom = require('phantom');
 const _ = require('lodash')
 const fs = require('fs')
 
+let pageCount = 0
+
 async function main(){
-  const maxPage = 5
+  const maxPage = 5 // 抓取的页面数
   const pageStarts = _.range(5).map(item => item *50)
+  // 放入一个数组 开始并行执行请求
   const proArr = pageStarts.map(item => getPage(item))
   try {
     let res = await Promise.all(proArr)
     res.forEach(element => {
-      fs.writeFile('note' + element.start, element.content)
+      // fs.writeFile(__dirname + 'dist/' + 'note' + element.start, element.content)
     });
   } catch (error) {
     console.log(error)
@@ -30,6 +33,8 @@ async function getPage(start) {
   const status = await page.open(config.url + start);
   const content = await page.property('content');
   await instance.exit();
+  pageCount++
+  console.log(`已经抓取${pageCount}页,本页面数据范围是${start}-${start + 50}`)
   return {content, start};
 }
 main()
