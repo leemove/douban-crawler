355cbf8e04d212d7675d833fe1a0a71ca557e83c
leemove <923522454@qq.com>
2018-03-23 17:14:40 +0800

    增加数据整理

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
 const config = require('./config.json')
-const r = require('request-promise-native')
-const phantom = require('phantom');
+const phantom = require('phantom')
 const _ = require('lodash')
 const fs = require('fs')
+const cheerio = require('cheerio')
+// var mongoose = require('mongoose')
+// mongoose.connect('mongodb://localhost:27017');
+// var db = mongoose.connection;
+// db.once('open', function() {
+//   console.log('数据库连接')
+// });
 
 let pageCount = 0
 
 async function main(){
-  const maxPage = 5 // 抓取的页面数
-  const pageStarts = _.range(5).map(item => item *50)
+  const maxPage = 1 // 抓取的页面数
+  const pageStarts = _.range(maxPage).map(item => item *50)
   // 放入一个数组 开始并行执行请求
   const proArr = pageStarts.map(item => getPage(item))
   try {
     let res = await Promise.all(proArr)
     res.forEach(element => {
+    const $ = cheerio.load(element.content)
+    const trs = $('table.olt td.title a')
+    trs.each((index, element) => {
+      element = cheerio(element)
+      const title = element.attr('title')
+      const herf = element.attr('href')
+      const parent = element.parent().parent()
+      const timeStr = cheerio(parent).find('td.time').text()
+      const time = new Date('2018-' + timeStr)
+      console.log({title, herf, time})
+    })
       // fs.writeFile(__dirname + 'dist/' + 'note' + element.start, element.content)
     });
   } catch (error) {
@@ -37,4 +54,6 @@ async function getPage(start) {
   console.log(`已经抓取${pageCount}页,本页面数据范围是${start}-${start + 50}`)
   return {content, start};
 }
+
+
 main()
